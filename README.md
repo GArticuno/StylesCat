e3a1a647197eb434662744e4097e3b2b1d5f4f5a
Mateus Romão <m.romao.ds@gmail.com>
2021-06-07 17:31:21 -0300

    New style

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # StylesCat
-Glassmorphism, Dark Mode and Bauhaus style.
+Black and White, glassmorphism, dark mode, bauhaus and neon style.
 
 ![alt text](https://github.com/GArticuno/StylesCat/blob/main/Stylecat.png?raw=true)
--- script.js
+++ script.js
@@ -1,101 +1,58 @@
+var body = document.querySelector('body');
+var header = document.querySelector('header');
+var main = document.querySelector('main');
+var cat = document.querySelector('.cat');
+var footer = document.querySelector('footer');
 
-var a = document.querySelector('.horas')
-var cat = document.querySelector('#cat');
-var globo = document.querySelector('#globo');
-
-var body = document.querySelector('.body')
-var header = document.querySelector('.header');
-var main = document.querySelector('.main');
-var footer = document.querySelector('.footer');
-
-var s = document.querySelector('.site');
-var tmhora =document.querySelector('.tmhora');
-var tmimage = document.querySelector('.tmimage');
-var tmfinal = document.querySelector('.tmfinal');
-var animacao= document.querySelector('#animacao');
-
+function bw(){
+    body.style.backgroundImage= "none";
+    header.setAttribute('id','bw');
+    main.setAttribute('id','bw');
+    cat.setAttribute('id','bw');
+    main.style.backgroundImage = "none";
+    cat.style.color="#202020";
+    cat.innerHTML="h i j";
+    footer.setAttribute('id','bw');
+}
 function glass(){
-    s.style.backgroundImage = "url('imgs/tmglass.png')";
-    tmhora.setAttribute('id','glass');
-    body.style.backgroundImage= "url('imgs/glass.png')";
+    body.style.backgroundImage= "url('assets/imgs/glass.png')";
     header.setAttribute('id','glass');
     main.setAttribute('id','glass');
-    main.style.backgroundImage = "url('imgs/tmglass.png')";
+    cat.setAttribute('id','glass');
+    main.style.backgroundImage = "url('assets/imgs/tmglass.png')";
+    cat.style.color="#4863ff";
+    cat.style.backgroundColor="none";
+    cat.innerHTML="l m n";
     footer.setAttribute('id','glass');
-    tmimage.setAttribute('id','glass');
-    cat.style.color="#ff7bbf";
-    cat.innerHTML="m";
-    globo.src='imgs/terra-glass.png';
-    tmfinal.setAttribute('id','glass');
-    animacao.src = 'imgs/imgglass.png';
 }
 function dark(){
-    s.style.backgroundImage="url('imgs/tmdark.png')"
-    body.style.backgroundImage ="url('imgs/dark.png')"
-    header.setAttribute('id','tmdark');
-    main.setAttribute('id','tmdark');
-    main.style.backgroundImage="url('imgs/tmdark.png')"
-    footer.setAttribute('id','tmdark');
-    tmhora.setAttribute('id','darkb');
-    tmimage.setAttribute('id','darkb');
-    cat.style.color="#964ec2";
-    cat.innerHTML="w";
-    globo.src='imgs/terra-dark.png';
-    tmfinal.setAttribute('id','darkb');
-    animacao.src = 'imgs/imgdark.png';
+    body.style.backgroundImage ="url('assets/imgs/dark.png')";
+    header.setAttribute('id','dark');
+    main.setAttribute('id','dark');
+    main.style.backgroundImage="url('assets/imgs/tmdark.png')";
+    cat.setAttribute('id','dark');
+    cat.style.color="#ffffff";
+    cat.style.backgroundColor="none";
+    cat.innerHTML="v w x";
+    footer.setAttribute('id','dark');
 }
 function bauhaus(){
-    s.style.backgroundImage = "url('imgs/bauhaus2.png')";
-    tmhora.setAttribute('id','tmbauhaus');
-    body.style.backgroundImage= "url('imgs/tmbauhaus.png')";
-    header.setAttribute('id','tmbauhaus');
-    main.setAttribute('id','glass');
-    main.style.backgroundImage = "url('imgs/bauhaus.png')";
-    footer.setAttribute('id','tmbauhaus');
-    tmimage.setAttribute('id','bauhausimg');
-    cat.style.color="rgb(247, 94, 94)";
-    cat.innerHTML="r";
-    globo.src='imgs/terra-bauhaus.png';
-    tmfinal.setAttribute('id','bauhausfinal');
-    animacao.src = 'imgs/imgbauhaus.png';
+    body.style.backgroundImage= "url('assets/imgs/tmbauhaus.png')";
+    header.setAttribute('id','bauhaus');
+    main.setAttribute('id','bauhaus');
+    cat.setAttribute('id','bauhaus');
+    main.style.backgroundImage = "url('assets/imgs/bauhaus.png')";
+    cat.style.color="#39db44";
+    cat.innerHTML="b c d";
+    footer.setAttribute('id','bauhaus');
 }
-function horario(){
-    
-    var data =new Date().toLocaleDateString();
-    var hora = new Date().getHours();
-    var minutos = new Date().getMinutes();
-    var dia= new Date().getDay();
-    if(hora<=9){
-        hora='0'+hora;
-    }
-    if(minutos<=9){
-        minutos='0'+minutos;
-    }
-    switch(dia){
-        case 1:
-            dia="Segunda-feira";
-            break;
-        case 2:
-            dia="Terça-feira";
-            break;
-        case 3:
-            dia="Quarta-feira";
-            break;  
-        case 4:
-            dia="Quinta-feira";
-            break;
-        case 5:
-            dia="Sexta-feira";
-            break;
-        case 6:
-            dia="Sábado";
-            break;
-        case 7:
-            dia="Domingo";
-            break;
-        }
-    a.innerHTML = "<p id='horario'>"+ hora +':'+minutos+'</p>'+ 
-                "<p id='data'>"+data+'</p>'+
-                "<p id='dia'>"+dia+'</p>';
-    setTimeout(horario, 1000)
+function neon(){
+    body.style.backgroundImage= "url('assets/imgs/dark.png')";
+    header.setAttribute('id','neon');
+    main.setAttribute('id','neon_btn');
+    cat.setAttribute('id','neon_btn');
+    main.style.backgroundImage = "url('assets/imgs/tmdark.png')";
+    cat.style.color="#f130d1";
+    cat.innerHTML="p q r";
+    footer.setAttribute('id','neon');
 }
